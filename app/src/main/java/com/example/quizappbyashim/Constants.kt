388600091dacd83cbdf8a5864c698cb3e23399f4
Ashim <ashim4673@gmail.com>
2022-10-22 19:08:09 +0530

    drawing app

---
+++ app/src/main/java/com/example/quizappbyashim/Constants.kt
@@ -0,0 +1,271 @@
+package com.example.quizappbyashim
+
+object Constants {
+
+    const val USER_NAME :String="user_name"
+    const val TOTAL_QUESTIONS: String= "total_qustions"
+    const val CORRECT_ANSWERS = "correct_answers"
+
+    fun getQuestions():ArrayList<Question>{
+        val questionsList=ArrayList<Question>()
+
+        val que1=Question(
+            1,"Which Marvel Character it is ?",
+            R.drawable.black_panther,
+            "Wolf Man","Black Adam","Black Panther","Batman",
+            3
+        )
+        questionsList.add(que1)
+
+        val que2=Question(
+            2,"Which Marvel Character it is ?",
+            R.drawable.shang_chi,
+            "Storm Shadow","Shang chi","Lord of Rings","Iron man",
+            2
+        )
+        questionsList.add(que2)
+
+        val que3=Question(
+            3,"Which Marvel Character it is ?",
+            R.drawable.wolverine,
+            "Deadpool","Professor X","X men","Wolverine",
+            4
+        )
+        questionsList.add(que3)
+
+        val que4=Question(
+            4,"Which Marvel Character it is ?",
+            R.drawable.ghost_rider,
+            "Flash","Ghost Rider","FireStar","Human Torch",
+            2
+        )
+        questionsList.add(que4)
+
+        val que5=Question(
+            5,"Which Marvel Character it is ?",
+            R.drawable.falcon,
+            "Phoenix","Falcon","The Wasp","Captain America",
+            2
+        )
+        questionsList.add(que5)
+
+        val que6=Question(
+            6,"Which Marvel Character it is ?",
+            R.drawable.magneto,
+            "Magneto","Dr. Octopus","Juggernaut","Iron Man",
+            1
+        )
+        questionsList.add(que6)
+
+        val que7=Question(
+            7,"Which Marvel Character it is ?",
+            R.drawable.valkarie,
+            "Natasha Romanov","Valkarie","Captain Marvel","Wanda Maximoff",
+            2
+        )
+        questionsList.add(que7)
+
+        val que8=Question(
+            8,"Which Marvel Character it is ?",
+            R.drawable.mr_fantastic,
+            "Steve Rogers","Dr. Strange","Mr. Fantastic","Tony Stark",
+            3
+        )
+        questionsList.add(que8)
+
+        val que9=Question(
+            9,"Which Marvel Character it is ?",
+            R.drawable.storm,
+            "Mephisto","Nozel Silva","Killer Frost","Storm",
+            4
+        )
+        questionsList.add(que9)
+
+        val que10=Question(
+            10,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que10)
+
+        val que11=Question(
+            13,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que11)
+
+        val que12=Question(
+            12,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que12)
+
+        val que13=Question(
+            13,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que13)
+
+        val que14=Question(
+            14,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que14)
+
+        val que15=Question(
+            15,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que15)
+        val que16=Question(
+            16,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que16)
+
+        val que17=Question(
+            17,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que17)
+
+        val que18=Question(
+            18,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que18)
+
+        val que19=Question(
+            19,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que19)
+
+        val que20=Question(
+            20,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que20)
+
+        val que21=Question(
+            21,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que21)
+
+        val que22=Question(
+            22,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que22)
+
+        val que23=Question(
+            23,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que23)
+
+        val que24=Question(
+            24,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que24)
+
+        val que25=Question(
+            25,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que25)
+
+        val que26=Question(
+            26,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que26)
+
+        val que27=Question(
+            27,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que27)
+
+        val que28=Question(
+            28,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que28)
+
+        val que29=Question(
+            29,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que29)
+
+        val que30=Question(
+            30,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que30)
+
+        val que31=Question(
+            31,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que31)
+
+        val que32=Question(
+            32,"Which Marvel Character it is ?",
+            R.drawable.vision,
+            "Ultron","Professor X","Vision","Captain America",
+            3
+        )
+        questionsList.add(que32)
+
+
+
+    return questionsList
+    }
+}
